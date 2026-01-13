d054089319ef93b702d4c8a8127e93d4d1f09d52
David <twdeveloper@outlook.com>
2026-01-13 16:45:18 +0800

    sample

---
+++ EchoBot1/Bots/ChatGPT.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace isRock.Template
+{
+    [JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
+    public enum Role
+    {
+        assistant, user, system
+    }
+
+    public class ChatGPT
+    {
+        const string AzureOpenAIEndpoint = "https://testaoai20250113.openai.azure.com";  //👉replace it with your Azure OpenAI Endpoint
+        const string AzureOpenAIModelName = "/gpt-4o"; //👉repleace it with your Azure OpenAI Model Deploy Name
+        const string AzureOpenAIToken = "1B5nF8INNeFg4EVbtLHaMSOabDz3CWzHOuvxv0r9jGoqaqJRHgPvJQQJ99CAACYeBjFXJ3w3AAABACOGr8YP"; //👉repleace it with your Azure OpenAI API Key
+        const string AzureOpenAIVersion = "2025-01-01-preview";  //👉replace  it with your Azure OpenAI API Version
+
+        public static string CallAzureOpenAIChatAPI(
+            string endpoint, string modelName, string apiKey, string apiVersion, object requestData)
+        {
+            var client = new HttpClient();
+
+            // 設定 API 網址
+            var apiUrl = $"{endpoint}/openai/deployments/{modelName}/chat/completions?api-version={apiVersion}";
+
+            // 設定 HTTP request headers
+            client.DefaultRequestHeaders.Add("api-key", apiKey);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));//ACCEPT heade
+            // 將 requestData 物件序列化成 JSON 字串
+            string jsonRequestData = Newtonsoft.Json.JsonConvert.SerializeObject(requestData);
+            // 建立 HTTP request 內容
+            var content = new StringContent(jsonRequestData, Encoding.UTF8, "application/json");
+            // 傳送 HTTP POST request
+            var response = client.PostAsync(apiUrl, content).Result;
+            // 取得 HTTP response 內容
+            var responseContent = response.Content.ReadAsStringAsync().Result;
+            var obj = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(responseContent);
+            return obj.choices[0].message.content.Value;
+        }
+
+
+        public static string getResponseFromGPT(string Message, List<Message> chatHistory)
+        {
+            //建立對話紀錄
+            var messages = new List<ChatMessage>
+                    {
+                        new ChatMessage {
+                            role = Role.system ,
+                            content = @"
+                               你是一個售票客服機器人，客戶會向你購票，你必須從客戶的購票敘述中找到底下這些購票資訊。
+                               找到的資訊必須覆述一次，如果有缺少的資訊，必須提醒客戶缺少的部分，直到蒐集完所有資訊後，
+                               要跟客戶做最後的確認，並且問客戶是否正確? 
+                               如果客戶回答不正確，則要重新蒐集資訊。
+                               如果客戶說正確，則將蒐集到的資料，整理成一個JSON字串，直接輸出，無須回覆其他文字。
+                               
+                               購票資訊包含:
+                                * 乘車起始站(start)
+                                * 乘車目的站(target)     
+                                * 乘車預計出發時間(start_time)
+                                * 乘車張數(amout)
+                                * 乘車票種(type)
+
+                               票種包含:
+                                * 全票
+                                * 學生票
+                                * 愛心票
+                                * 自由座
+
+                                回應時，請注意以下幾點:
+                                * 不要說 '感謝你的來信' 之類的話，因為客戶是從對談視窗輸入訊息的，不是寫信來的
+                                * 不要一直說抱歉或對不起，但可以說不好意思。
+                                * 不要幫客戶購票，也不要說跟金額有關的事情
+                                * 要能夠盡量解決客戶的問題
+                                * 不要以回覆信件的格式書寫，請直接提供對談機器人可以直接給客戶的回覆
+                                ----------------------
+"
+                        }
+                    };
+
+            //添加歷史對話紀錄
+            foreach (var HistoryMessageItem in chatHistory)
+            {
+                //添加一組對話紀錄
+                messages.Add(new ChatMessage()
+                {
+                    role = Role.user,
+                    content = HistoryMessageItem.UserMessage
+                });
+                messages.Add(new ChatMessage()
+                {
+                    role = Role.assistant,
+                    content = HistoryMessageItem.ResponseMessage
+                });
+            }
+            //add user message
+            messages.Add(new ChatMessage()
+            {
+                role = Role.user,
+                content = Message
+            });
+            //回傳呼叫結果
+            return ChatGPT.CallAzureOpenAIChatAPI(
+               AzureOpenAIEndpoint, AzureOpenAIModelName, AzureOpenAIToken, AzureOpenAIVersion,
+                new
+                {
+                    //model = "gpt-3.5-turbo",
+                    messages = messages
+                }
+             );
+        }
+    }
+
+    public class ChatMessage
+    {
+        public Role role { get; set; }
+        public string content { get; set; }
+    }
+
+}
---
+++ EchoBot1/Bots/EchoBot.cs
@@ -0,0 +1,53 @@
+﻿// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.22.0
+
+using isRock.Template;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EchoBot1.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            //var replyText = isRock.Template.ChatGPT.getResponseFromGPT(turnContext.Activity.Text,.....);
+            //var replyText = $"Echo: {turnContext.Activity.Text}";
+
+            var replyText = "";
+
+            if (turnContext.Activity.Text.Contains("/reset"))
+            {
+                ChatHistoryManager.DeleteIsolatedStorageFile();
+                replyText = "我已經把之前的對談都給忘了!";
+            }
+            else
+            {
+                var chatHistory = ChatHistoryManager.GetMessagesFromIsolatedStorage("UserA");
+                //var replyText = $"Echo: {turnContext.Activity.Text}";
+                replyText = ChatGPT.getResponseFromGPT(turnContext.Activity.Text, chatHistory);
+                //儲存聊天紀錄
+                ChatHistoryManager.SaveMessageToIsolatedStorage(
+                    DateTime.Now, "UserA", turnContext.Activity.Text, replyText);
+            }
+
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
